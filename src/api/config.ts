42e2a55bb9e64c18dcc4eaa46930efff5f5d8c48
sairajB <sairaj.sab@gmail.com>
2025-05-04 09:23:40 +0530

    API key for testing

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,9 +1,9 @@
 export const API_CONFIG = {
   BASE_URL: "https://api.openweathermap.org/data/2.5",
   GEO: "https://api.openweathermap.org/geo/1.0",
-  API_KEY: import.meta.env.VITE_OPENWEATHER_API_KEY,
+  API_KEY: "639d84b48fe5eea038693a9577367852",
   DEFAULT_PARAMS: {
     units: "metric",
-    appid: import.meta.env.VITE_OPENWEATHER_API_KEY,
+    appid: "639d84b48fe5eea038693a9577367852",
   },
 };
